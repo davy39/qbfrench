d15611f2a0adea795808bc82c994fb054bd84014
Simon Brulhart <simon@brulhart.me>
2016-08-11 18:12:34 +0200

    Update torrent9 plugin
    * Update scraping code for new website layout
    * Refactor for py2+3 compatibility
    * Follow PEP8

--- torrent9.py
+++ torrent9.py
@@ -1,77 +1,122 @@
 # -*- coding: utf-8 -*-
-#VERSION: 1.0
+#VERSION: 1.1
 #AUTHOR: Davy39 <davy39@hmamail.com>
+#CONTRIBUTORS: Simon <simon@brulhart.me>
 
-#Copyleft
+# Copyleft
 
+from __future__ import print_function
+
+import re
+try:
+    # python2
+    from HTMLParser import HTMLParser
+except ImportError:
+    # python3
+    from html.parser import HTMLParser
+
+from helpers import download_file, retrieve_url
 from novaprinter import prettyPrinter
-import urllib2, tempfile, sgmllib, os, re
 
-class cpasbien(object):
-  url = "http://www.cpasbien.pe"
-  name = "Cpasbien (french)"
-  supported_categories = {"all": "", "books": "ebook/", "movies": "films/", "tv": "series/", "music": "musique/", "software": "logiciels/", "games": ""}
-  
-  def __init__(self):
-    self.results = []
-    self.parser = self.SimpleSGMLParser(self.results, self.url)
-  
-  def download_torrent(self, url):
-    file, path = tempfile.mkstemp(".torrent")
-    file = os.fdopen(file, "wb")
-    dat = urllib2.urlopen(urllib2.Request(url, headers={"User-Agent" : "Mozilla/5.0"} )).read()
-    file.write(dat)
-    file.close()
-    print path+" "+url
-    
-  class SimpleSGMLParser(sgmllib.SGMLParser):
-    def __init__(self, results, url, *args):
-      sgmllib.SGMLParser.__init__(self)
-      self.url = url
-      self.data_counter = None
-      self.current_item = None
-      self.results = results
-    
-    def start_a(self, attr):
-      params = dict(attr)
-      if params.has_key("href") and params["href"].startswith("http://www.cpasbien.pe/dl-torrent"):
-	self.current_item = {}
-        self.data_counter = 0
-        self.current_item["desc_link"]=params["href"].strip()
-        self.current_item["link"] = "http://www.cpasbien.pe/dl_torrent.php?permalien="+str(params["href"].strip().split("/")[6].split(".")[0])
 
-    def handle_data(self, data):
-	if isinstance(self.data_counter,int):
-          self.data_counter += 1
-          if self.data_counter == 3:
-            self.current_item["name"] = data.strip()
-          elif self.data_counter == 6:
-            self.current_item["size"] = data.strip()
-          elif self.data_counter == 9:
-            self.current_item["seeds"] = data.strip()
-          elif self.data_counter == 11:
-            self.current_item["leech"] = data.strip()
-            self.current_item["engine_url"] = self.url
-            self.data_counter = None
-            prettyPrinter(self.current_item)
-            self.results.append("a")
+class torrent9(object):
+    url = "http://www.torrent9.tv"
+    name = "Torrent9 (french)"
+    supported_categories = {
+        "all": [""],
+        "books": ["ebook/"],
+        "movies": ["films/"],
+        "tv": ["series/"],
+        "music": ["musique/"],
+        "software": ["logiciels/"],
+        "games": ["jeux-pc/", "jeux-consoles/"]
+    }
 
-  def search(self, what, cat="all"):
-    i = 0
-    while i<50:
-      results = []
-      parser = self.SimpleSGMLParser(results, self.url)
-      if cat == "games":
-        dat = urllib2.urlopen(urllib2.Request(self.url+"/recherche/jeux-pc/%s/page-%d,trie-seeds-d"%(what, i), headers={"User-Agent" : "Mozilla/5.0"} )).read().replace(" & ", " et ")
-        dat += urllib2.urlopen(urllib2.Request(self.url+"/recherche/jeux-consoles/%s/page-%d,trie-seeds-d"%(what, i), headers={"User-Agent" : "Mozilla/5.0"} )).read().replace(" & ", " et ")
-      else:
-      	dat = urllib2.urlopen(urllib2.Request(self.url+"/recherche/%s%s/page-%d,trie-seeds-d"%(self.supported_categories[cat], what, i), headers={"User-Agent" : "Mozilla/5.0"} )).read().replace(" & ", " et ")
-      results_re = re.compile('(?s)<tbody>.*')
-      for match in results_re.finditer(dat):
-        res_tab = match.group(0)
-        parser.feed(res_tab)
-        parser.close()
-        break
-      if len(results) <= 0:
-        break
-      i += 1 
+    def __init__(self):
+        self.results = []
+        self.parser = self.SimpleHTMLParser(self.results)
+
+    def download_torrent(self, url):
+        print(download_file(url))
+
+    class SimpleHTMLParser(HTMLParser):
+        def __init__(self, results):
+            HTMLParser.__init__(self)
+            self.td_counter = None
+            self.current_item = None
+            self.collect_seeds = False
+            self.results = results
+
+        def handle_starttag(self, tag, attr):
+            method = 'start_' + tag
+            if hasattr(self, method) and tag in ('a', 'span', 'td'):
+                getattr(self, method)(attr)
+
+        def start_a(self, attr):
+            params = dict(attr)
+            if params.get('href', '').startswith('/torrent/') and 'title' in params:
+                self.current_item = {}
+                self.td_counter = 0
+                self.current_item['engine_url'] = torrent9.url
+                desc_path = params['href'].strip()
+                self.current_item['desc_link'] = torrent9.url + desc_path
+                self.current_item['link'] = (
+                    torrent9.url
+                    + desc_path.replace('/torrent/', '/get_torrent/')
+                    + '.torrent'
+                )
+                m = re.match(
+                    u'(?:Télécharger )(.*)(?: en Torrent)', params['title'],
+                    flags=re.IGNORECASE
+                )
+                if m:
+                    self.current_item['name'] = m.group(1)
+                else:
+                    self.current_item['name'] = params['title']  # Fallback
+
+        def start_span(self, data):
+            if self.current_item and self.td_counter == 2:
+                self.collect_seeds = True
+
+        def start_td(self, data):
+            if self.current_item:
+                self.td_counter += 1
+
+        def handle_data(self, data):
+            if self.current_item and isinstance(self.td_counter, int):
+                if self.td_counter == 1 and 'size' not in self.current_item:
+                    self.current_item['size'] = unit_fr2en(data.strip())
+                elif self.collect_seeds:
+                    self.collect_seeds = False
+                    self.current_item['seeds'] = data.strip()
+                elif self.td_counter == 3 and 'leech' not in self.current_item:
+                    self.current_item["leech"] = data.strip()
+
+        def handle_endtag(self, tag):
+            if self.current_item and tag == 'tr':
+                prettyPrinter(self.current_item)
+                self.results.append('a')
+                self.current_item = None
+
+    def search(self, what, cat="all"):
+        for page in range(35):
+            results = []
+            parser = self.SimpleHTMLParser(results)
+            for subcat in self.supported_categories[cat]:
+                data = retrieve_url(
+                    '{}/search_torrent/{}{}/page-{},trie-seeds-d'
+                    .format(self.url, subcat, what, page)
+                )
+                parser.feed(data)
+            parser.close()
+            if len(results) <= 0:
+                break
+
+
+def unit_fr2en(size):
+    """Convert french size unit to english"""
+    return re.sub(
+        r'([KMGTP])o',
+        lambda match: match.group(1) + 'B',
+        size, flags=re.IGNORECASE
+    )
